948b512466d1cebf60d2530a241879e504b7d881
Joao Pinto <joaobose@gmail.com>
2020-12-25 15:12:44 -0400

    JSDoc and going back to fun.throw

--- functions/general/JWT.fun.js
+++ functions/general/JWT.fun.js
@@ -1,47 +1,88 @@
 const jwt = require('jsonwebtoken');
 const fun = require('./errors.fun');
+const BaseError = require('../../classes/src/BaseError');
 
 const Errors = require('../../classes/src/errors');
 
+/**
+ * Decodes an http Bearer JWT token authentication from an express request.
+ *
+ * @since  0.3.0
+ *
+ * @param {Express.Request}       req      An express request
+ * @param {Express.Response}      res      An express response
+ *
+ * @throws {BaseError} If an error occurs while decoding the scheme. This errors gets automatically sent to the response using fun.throw.
+ *
+ * @returns {{decoded: any, token: string}} The given bearer token and the decoded JWT content
+ */
 let httpDecodeBearerScheme = (req, res) => {
+  // ---------------------- getting the Auth -------------------- //
+  let auth = req.get('Authorization');
+  if (!auth) {
+    let reason = 'Missing authorization.';
+    fun.throw(req, res, new Errors.UnauthorizedError(reason));
+  }
+  auth = auth.trim();
+  let authSplit = auth.split(' ');
+
+  // ------------------- validating auth scheme ----------------- //
+  let authScheme = authSplit[0];
+  if (authScheme != 'Bearer') {
+    let reason = 'Wrong auth scheme, expected Bearer. given: ' + authScheme;
+    fun.throw(req, res, new Errors.UnauthorizedError(reason));
+  }
+
+  // ------------------ validating the token -------------------- //
+  let token = authSplit[1];
+  if (!token) {
+    fun.throw(req, res, new Errors.JWT.MissingJWTError());
+  }
+
+  // ------------------ decoding the Auth JWT ------------------- //
   try {
-    // ---------------------- getting the Auth -------------------- //
-    let auth = req.get('Authorization');
-    if (!auth) {
-      let reason = 'Missing authorization.';
-      throw new Errors.UnauthorizedError(reason);
-    }
-    auth = auth.trim();
-    let authSplit = auth.split(' ');
-
-    // ------------------- validating auth scheme ----------------- //
-    let authScheme = authSplit[0];
-    if (authScheme != 'Bearer') {
-      let reason = 'Wrong auth scheme, expected Bearer. given: ' + authScheme;
-      throw new Errors.UnauthorizedError(reason);
-    }
-
-    // ------------------ validating the token -------------------- //
-    let token = authSplit[1];
-    if (!token) {
-      throw new Errors.JWT.MissingJWTError();
-    }
-
-    // ------------------ decoding the Auth JWT ------------------- //
     let decoded = jwt.decode(token);
     if (!decoded) {
-      throw new Errors.JWT.BadJWTError(token);
+      fun.throw(req, res, new Errors.JWT.BadJWTError(token));
     }
 
     return {
       decoded: decoded,
       token: token
     };
-  } catch (e) {
-    fun.throw(req, res, e);
+  } catch {
+    fun.throw(req, res, new Errors.JWT.BadJWTError(token));
   }
 };
 
+/**
+ * Verifies an user's http JWT scheme using the decoded content and a source funtion.
+ *
+ * @since  0.3.0
+ *
+ * @async
+ *
+ * @param {Express.Request}       req                       An express request
+ * @param {Express.Response}      res                       An express response
+ *
+ * @param {{decoded: any,
+ *          token: string}}       schema                    The JWT decoded scheme
+ *
+ * @param {(identifier: any) =>
+ *        Promise<{
+ *          secret: string
+ *        }>}                     source                    The source function for the user's JWT secret. You can use this function to fetch the user's JWT secret from your database or storage.
+ *
+ * @param {{
+ *    indexingProperty: string,
+ *    indexRejectionFunction:
+ *      (identifier: any) =>
+ *        boolean
+ *    }}                         [options]                  Options. ```indexingProperty``` is the name of the property that holds the user identifier, default is ```'user'```. ```indexRejectionFunction``` is a boolean rejection function to invalidate the decoded identifier. Use this to handle ```undefined```, ```null``` or invalid identifiers, default is ```isNaN```.
+ *
+ *
+ * @throws {BaseError} If an error occurs while verifying the scheme. This error gets automatically sent to the response using fun.throw.
+ */
 let httpVerifyUserToken = async (
   req,
   res,
@@ -59,23 +100,44 @@ let httpVerifyUserToken = async (
   });
 
   // --------------- getting the valid JWT secret ---------------- //
-  try {
-    let validJWT = await source(id);
-    if (!validJWT) {
-      throw new Errors.JWT.InvalidJWTError(schema.token);
-    }
+  let validJWT = await source(id);
+  if (!validJWT) {
+    fun.throw(req, res, new Errors.JWT.InvalidJWTError(schema.token));
+  }
 
-    // -------------------- verifying the JWT -------------------- //
-    try {
-      jwt.verify(schema.token, validJWT.secret);
-    } catch {
-      throw new Errors.JWT.InvalidJWTError(schema.token);
-    }
-  } catch (e) {
-    fun.throw(req, res, e);
+  // -------------------- verifying the JWT -------------------- //
+  try {
+    jwt.verify(schema.token, validJWT.secret);
+  } catch {
+    fun.throw(req, res, new Errors.JWT.InvalidJWTError(schema.token));
   }
 };
 
+/**
+ * Validates and returns the user identifier from the http JWT scheme.
+ *
+ * @since  0.3.0
+ *
+ * @async
+ *
+ * @param {Express.Request}        req                       An express request
+ * @param {Express.Response}       res                       An express response
+ *
+ * @param {{decoded: any,
+ *          token: string}}       schema                    The JWT decoded scheme
+ *
+ * @param {{
+ *    indexingProperty: string,
+ *    indexRejectionFunction:
+ *      (identifier: any) =>
+ *        boolean
+ *    }}                         [options]                  Options. ```indexingProperty``` is the name of the property that holds the user identifier, default is ```'user'```. ```indexRejectionFunction``` is a boolean rejection function to invalidate the decoded identifier. Use this to handle ```undefined```, ```null``` or invalid identifiers, default is ```isNaN```.
+ *
+ *
+ * @throws {BaseError} If an error occurs while verifying the scheme. This error gets automatically sent to the response using fun.throw.
+ *
+ * @returns {any} The user identifier.
+ */
 let httpValidateUserIdentifierFromSchema = (
   req,
   res,
@@ -92,6 +154,17 @@ let httpValidateUserIdentifierFromSchema = (
   return id;
 };
 
+/**
+ * Decodes a Bearer JWT token authentication from an authorization string.
+ *
+ * @since  0.3.0
+ *
+ * @param {string}                authorization      An authorization string.
+ *
+ * @throws {string} If an exception occurs while decoding the scheme. The error ```string``` shows the reason of the exception.
+ *
+ * @returns {{decoded: any, token: string}} The given bearer token and the decoded JWT content.
+ */
 let decodeBearerScheme = (authorization) => {
   // ---------------------- getting the Auth -------------------- //
   let auth = authorization.trim();
@@ -110,17 +183,46 @@ let decodeBearerScheme = (authorization) => {
   }
 
   // ------------------ decoding the Auth JWT ------------------- //
-  let decoded = jwt.decode(token);
-  if (!decoded) {
+  try {
+    let decoded = jwt.decode(token);
+    if (!decoded) {
+      throw 'Bad JWT.';
+    }
+
+    return {
+      decoded: decoded,
+      token: token
+    };
+  } catch {
     throw 'Bad JWT.';
   }
-
-  return {
-    decoded: decoded,
-    token: token
-  };
 };
 
+/**
+ * Verifies an user's JWT scheme using the decoded content and a source funtion.
+ *
+ * @since  0.3.0
+ *
+ * @async
+ *
+ * @param {{decoded: any,
+ *          token: string}}       schema                    The JWT decoded scheme
+ *
+ * @param {(identifier: any) =>
+ *        Promise<{
+ *          secret: string
+ *        }>}                     source                    The source function for the user's JWT secret. You can use this function to fetch the user's JWT secret from your database or storage.
+ *
+ * @param {{
+ *    indexingProperty: string,
+ *    indexRejectionFunction:
+ *      (identifier: any) =>
+ *        boolean
+ *    }}                         [options]                  Options. ```indexingProperty``` is the name of the property that holds the user identifier, default is ```'user'```. ```indexRejectionFunction``` is a boolean rejection function to invalidate the decoded identifier. Use this to handle ```undefined```, ```null``` or invalid identifiers, default is ```isNaN```.
+ *
+ *
+ * @throws {string} If an error occurs while verifying the scheme. The error ```string``` shows the reason of the exception.
+ */
 let verifyUserToken = async (
   schema,
   source,
@@ -149,6 +251,28 @@ let verifyUserToken = async (
   }
 };
 
+/**
+ * Validates and returns the user identifier from the JWT scheme.
+ *
+ * @since  0.3.0
+ *
+ * @async
+ *
+ * @param {{decoded: any,
+ *          token: string}}       schema                    The JWT decoded scheme
+ *
+ * @param {{
+ *    indexingProperty: string,
+ *    indexRejectionFunction:
+ *      (identifier: any) =>
+ *        boolean
+ *    }}                         [options]                  Options. ```indexingProperty``` is the name of the property that holds the user identifier, default is ```'user'```. ```indexRejectionFunction``` is a boolean rejection function to invalidate the decoded identifier. Use this to handle ```undefined```, ```null``` or invalid identifiers, default is ```isNaN```.
+ *
+ *
+ * @throws {BaseError} If an error occurs while verifying the scheme. The error ```string``` shows the reason of the exception.
+ *
+ * @returns {any} The user identifier.
+ */
 let validateUserIdentifierFromSchema = (
   schema,
   { indexingProperty, rejectionFunction } = {
