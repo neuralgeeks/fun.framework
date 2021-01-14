63799a61e54634fd385e05aca56bd30f51e016d9
Joao Pinto <joaobose@gmail.com>
2021-01-14 00:59:01 -0400

    Adding license

--- functions/general/errors.fun.js
+++ functions/general/errors.fun.js
@@ -5,6 +5,23 @@ const GenericInternalServerError = require('../../classes/src/errors/GenericInte
 const ExceptionAfterHeadersSentError = require('../../classes/src/errors/ExceptionAfterHeadersSentError');
 
 /**
+ * @license
+ * Copyright 2020 neuralgeeks LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
  * Returns a JSONAPI based error handler that throws a JSONAPI error and sends feedback
  * to the response.
  *
